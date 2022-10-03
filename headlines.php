79f0f689378a39cb93a583bdbae22fd56ea28ba7
Ugwu Obed <vjazzyvictor@gmail.com>
2022-10-03 14:26:57 +0100

    news headlines

---
+++ headlines.php
@@ -0,0 +1,7 @@
+<?php  
+use jcobhams\NewsApi\NewsApi;
+require_once('');
+$newsapi = new NewsApi('c8d0a6e46b054d63a5f49ee0e9021571');
+
+$data = $sources = $newsapi->getSources($category, $language, $country);
+?>
