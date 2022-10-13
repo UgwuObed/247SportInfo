3e21504b0a691f2a7cae2a690d511f92af2eda2b
Ugwu Obed <vjazzyvictor@gmail.com>
2022-10-13 14:56:44 +0100

    headline api

--- headlines.php
+++ headlines.php
@@ -4,4 +4,5 @@ require_once('');
 $newsapi = new NewsApi('c8d0a6e46b054d63a5f49ee0e9021571');
 
 $data = $sources = $newsapi->getSources($category, $language, $country);
+
 ?>
---
+++ http.php
@@ -0,0 +1,5 @@
+<?php 
+$url ="https://newsapi.org/v2/top-headlines?country=ng&apiKey=c8d0a6e46b054d63a5f49ee0e9021571";
+$collection_name = 'RapidAPI';
+$request_url = $url . '/' . $collection_name;
+?>
